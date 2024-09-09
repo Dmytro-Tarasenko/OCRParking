8ce566a73badc3541d0b13d19cbf9a619ef46458
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-09-09 14:27:16 +0300

    Update routers added to main

---
+++ admin/routes.py
@@ -0,0 +1,11 @@
+from fastapi.routing import APIRouter
+from fastapi.responses import HTMLResponse
+
+router = APIRouter(prefix='admin',
+                   default_response_class=HTMLResponse,
+                   include_in_schema=False)
+
+
+@router.get('/')
+def get_user_page(request):
+    pass
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import uvicorn
 
 from routes.auth_routes import router as auth_router
 from frontend.routes import router as front_router
+from user.routes import router as user_router
+
 
 app = FastAPI()
 
@@ -17,6 +19,7 @@ app.mount("/static", StaticFiles(directory=static_path), name='static')
 
 app.include_router(auth_router)
 app.include_router(front_router)
+app.include_router(user_router)
 
 
 if __name__ == "__main__":
