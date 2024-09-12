7f8023be5f31a3c1d9fdf8749094a0886b2e6a12
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-09-12 14:06:32 +0300

    Scripts update

--- migrations/Scripts/cars_202409121406.sql
+++ migrations/Scripts/cars_202409121406.sql
@@ -1,9 +1,9 @@
 INSERT INTO public.cars (user_id,car_plate) VALUES
-	 (3,'asd1234asd'),
-	 (3,'548asd'),
 	 (7,'AA1547DD'),
 	 (7,'LK980AS'),
 	 (7,'JFK1969'),
 	 (2,'OP23ASD'),
 	 (2,'JSNINJA'),
-	 (2,'PYTHONRLZ');
+	 (2,'PYTHONRLZ'),
+	 (3,'ASD1234ASD'),
+	 (3,'548ASD');
